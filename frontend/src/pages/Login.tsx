5c9bdde401424b0c7f82f071652ffb844f4d190b
Shuhei-pp <lotakotayota@gmail.com>
2022-03-12 14:13:34 +0900

    Act a form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import '../css/Login.css';
+import './css/Login.css';
 
 const Login = () =>{
   return (
     <div>
       <form name="login_form">
         <div className="login_form_top">
-          <h1>ログイン画面</h1>
-          <p>ユーーザID、パスワードをご入力の上、「ログイン」ボタンをクリックしてください。</p>
+          <h1>LOGIN</h1>
+          <p>UserID、Passwordをご入力の上、「LOGIN」ボタンをクリックしてください。</p>
         </div>
         <div className="login_form_btm">
-          <input type="id" name="user_id" placeholder="ユーザーIDを入力してください"/><br/>
-          <input type="password" name="password"placeholder="パスワードを入力してください"/>
+          <input type="id" name="user_id" placeholder="UserID"/>
+          <input type="password" name="password" placeholder="Password"/>
+          <input type="submit" name="botton" value="LOGIN"/>
         </div>
-        <button type="submit">ログイン</button>
       </form>
     </div>
   )
