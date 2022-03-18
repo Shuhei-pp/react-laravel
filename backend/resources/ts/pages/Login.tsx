9dbfdfd00c2b042d0f5b06776bee3178f116a71c
Shuhei-pp <lotakotayota@gmail.com>
2022-03-18 17:49:02 +0900

    フロントのログインのapi繋ぎこみ

--- backend/resources/ts/pages/Login.tsx
+++ backend/resources/ts/pages/Login.tsx
@@ -2,36 +2,44 @@ import React, { useState, useEffect }  from 'react';
 import axios from 'axios';
 import './css/Login.css';
 
+class Login extends React.Component{
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
 
-
-const getUser = () => {
-  axios.get("/api/user")
-    .then(res => {
-      console.log('ログイン済み');
-      console.log(res.data);
-
-    }).catch(err => {
-      console.log('ログインしてません');
-    })
+  Post() {
+    axios.get('/sanctum/csrf-cookie')
+      .then(response => {
+        axios.post('/api/login', this.state)
+          .then(() => {
+            console.log("成功や");
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      })
+  }
+  render() {
+    return (
+      <div className="login_background">
+        <form className="login_form" onSubmit={(event) => { this.Post() }}>
+          <div className="login_form_text">
+            <h1>Login</h1>
+            <p>メールアドレス、Passwordをご入力の上、「Login」ボタンをクリックしてください。</p>
+          </div>
+          <div className="login_form_input">
+            <input type="mail" onChange={(e) => this.setState({ email: e.target.value })} placeholder="mail" />
+            <input type="password" onChange={(e) => this.setState({ password: e.target.value })} placeholder="Password" />
+            <input type="submit" name="botton" value="Login" />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
-
-const Login = () => {
-  getUser();
-  return (
-    <div className="login_background">
-      <form className="login_form">
-        <div className="login_form_text">
-          <h1>Login</h1>
-          <p>メールアドレス、Passwordをご入力の上、「Login」ボタンをクリックしてください。</p>
-        </div>
-        <div className="login_form_input">
-          <input type="mail" name="mail" placeholder="mail"/>
-          <input type="password" name="password" placeholder="Password"/>
-          <input type="submit" name="botton" value="Login"/>
-        </div>
-      </form>
-    </div>
-  )
-} 
 
 export default Login;
