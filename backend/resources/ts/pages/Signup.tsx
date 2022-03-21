a006dd12a1e0711ad388e0bc42b932cc7770ad8b
Shuhei-pp <lotakotayota@gmail.com>
2022-03-21 11:34:28 +0900

    set State in Singup component.

--- backend/resources/ts/pages/Signup.tsx
+++ backend/resources/ts/pages/Signup.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './css/Signup.css';
+import axios from 'axios';
 
 const Signup = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const Signup = () => {
+    console.log({ name, email, password });
+    /*axios.get('/sanctum/csrf-cookie')
+      .then(()=>{
+        axios.post('api/signup', { name, email, password })
+          .then((res) => {
+            console.log(res);
+          }).catch((error) => {
+            console.log(error);
+          });
+      });*/
+  }
+
   return (
     <div className="signup_background">
-      <form className="signup_form">
+      <div className="signup_form">
         <div className="signup_form_text">
           <h1>Signup</h1>
           <p>UserName、UserID、Passwordをご入力の上、「Signup」ボタンをクリックしてください。</p>
         </div>
         <div className="signup_form_btm">
-          <input type="text" name="user_name" placeholder="User Name"/>
-          <input type="text" name="mail" placeholder="Mail"/>
-          <input type="password" name="password" placeholder="Password"/>
-          <input type="submit" name="botton" value="Signup"/>
+          <input type="text" onChange={(e) => { setName(e.target.value) }} placeholder="User Name"/>
+          <input type="text" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Mail"/>
+          <input type="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Password"/>
+          <input type="submit" onClick={()=>Signup()} value="Signup"/>
         </div>
-      </form>
+      </div>
     </div>
   )
 } 
